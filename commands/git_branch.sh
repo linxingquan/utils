b62fade035b013186c031010a21260a8bf8da145
Mike Lin <milin@wish.com>
2022-01-13 00:52:38 +0000

    set-upstream

--- commands/git_branch.sh
+++ commands/git_branch.sh
@@ -9,6 +9,9 @@ git checkout $from_branch
 git pull
 
 git checkout -b $to_branch $from_branch
+
 echo "pushing $to_branch"
 git push origin $to_branch
+
+git branch --set-upstream-to=origin/$to_branch $to_branch
 
